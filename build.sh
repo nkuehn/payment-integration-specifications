27ef1d9649c5b18f466102af739e1698c71886e6
nkuehn <nikolaus.kuehn@commercetools.com>
2015-12-01 23:45:49 +0100

    fixes yaml type separates install from build run

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-# requires node to be installed.
-# initial install:
-# npm install yaml-to-json -g
-# npm install -g swagger-tools
-
 yaml2json types --output types
+jsonlint types/*.json --quiet
 yaml2json Payment-Integration-Api.swagger.yml > Payment-Integration-Api.swagger.json
 swagger-tools validate Payment-Integration-Api.swagger.json
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# installs required dependencies to run tests
+# requires node to be installed.
+npm install yaml-to-json -g
+npm install swagger-tools -g
+npm install jsonlint -g
